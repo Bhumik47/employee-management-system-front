a4708f26bc7a856e13f5e39bdf3b9615fc236d0c
Bhumik47 <bhumikdehane@gmail.com>
2024-04-03 21:22:27 -0700

    base Url fix

--- src/common/constant.js
+++ src/common/constant.js
@@ -1,2 +1,2 @@
 export const BASE_URL =
-  /* "http://localhost:4000/api/v1/" */ "https://employee-management-system-4jq8.onrender.com";
+  "https://employee-management-system-4jq8.onrender.com/api/v1/"; // http://localhost:4000/api/v1/
